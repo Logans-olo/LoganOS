03b2896bbe8a2f58104273ca465b588094c09780
Logans-olo <lsolonche1@gmail.com>
2025-10-16 20:58:35 +0000

    VGA Buffer, as well as print macros are completed.

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,17 @@
 #![no_main]
 mod vga_buffer;
 use::core::panic::PanicInfo;
-static LOGAN: &[u8] = b"My name is Logan";
 
-#[panic_handler] 
-fn panic(_info: &PanicInfo) -> ! { 
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in LOGAN.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("I am Panicking");
     loop {}
 }
