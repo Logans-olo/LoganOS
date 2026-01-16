a917126f77947c157094f6bda24f5db47afb4115
Logan Solonche <lsolonch@purdue.edu>
2026-01-16 10:17:27 -0500

    Testing (Unit + Integration)

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -145,3 +145,30 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+/* Test cases for the VGA Buffer */
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+/* End of VGA Buffer Test cases */
